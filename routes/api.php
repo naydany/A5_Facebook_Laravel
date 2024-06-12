df2635ef59714f859e326ecc7042307372c222b3
Sim senrin <senrin.sim@student.passerellesnumeriques.org>
2024-06-12 09:02:46 +0700

    Router post craete

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 Route::get('/me', [AuthController::class, 'index'])->middleware('auth:sanctum');
+Route::get('/user/post', [AuthController::class, 'user_posts']);
+Route::get('/user/post/{id}', [AuthController::class, 'show_user_posts']);
+
+
+Route::get('/post', [PostController::class,'index']);
+Route::get('/post/show/{id}', [PostController::class, 'show']);
+Route::post('/post/create', [PostController::class, 'store']);
+Route::put('/post/edit/{id}', [PostController::class, 'update']);
+Route::delete('/post/delete/{id}', [PostController::class, 'destroy']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
