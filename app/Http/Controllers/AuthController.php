6e1d0a4f00a5a8300c466a4e1030797ee93f1536
Sim senrin <senrin.sim@student.passerellesnumeriques.org>
2024-06-12 09:24:08 +0700

    create delete update read post of user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,21 +6,62 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use App\Http\Resources\ShowUserPostsResource;
+use App\Http\Resources\UserPostsResource;
+
 
 class AuthController extends Controller
 {
+
+    
+    
+    public function register(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        $user_exist = User::where('email', $request->email)->first();
+        if($user_exist){
+            return response([
+                'message' => "Email Already Exist !",
+                'success' => false,
+            ]);
+        }
+        
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'email_verified_at' => now(),
+            'password' => Hash::make($request->password),
+            'remember_token' => Str::random(20),
+
+        ]);
+        $token = $user->createToken('auth_token')->plainTextToken;
+    
+        return response([
+            'message' => "Create has been success",
+            'success' => true,
+            'access_token'  => $token,
+            'user' => $user
+        ]);
+    }
+    
     public function login(Request $request): JsonResponse
     {
         $validator = Validator::make($request->all(), [
             'email'     => 'required|string|max:255',
             'password'  => 'required|string'
-          ]);
+        ]);
 
         if ($validator->fails()) {
             return response()->json($validator->errors());
         }
-
+ 
         $credentials = $request->only('email', 'password');
 
         if (!Auth::attempt($credentials)) {
@@ -39,6 +80,13 @@ class AuthController extends Controller
         ]);
     }
 
+    public function logout(Request $request){
+        $request->user()->currentAccessToken()->delete();
+        return response()->json([
+            'message' => 'User successfully logged out',
+        ],200);
+    }
+    
     public function index(Request $request)
     {
         $user = $request->user();
@@ -48,5 +96,23 @@ class AuthController extends Controller
             'message' => 'Login success',
             'data' =>$user,
         ]);
+    }
+    public function user_posts()
+    {
+        $users = User::all();
+        try{
+            return response()->json(['data' => UserPostsResource::collection($users), 'success'=>true, 'message'=>'get user post successfully', 'status'=>200]);
+        }catch (\Exception $e){
+            return response()->json(['data' => $e->getMessage(),'success'=>false,'message'=>$e->getMessage(),'status'=>404]);
+        }
+    }
+    public function show_user_posts(string|int $id)
+    {
+        try{
+            $user = User::findOrFail($id);
+            return response()->json(['data' => new ShowUserPostsResource($user), 'success'=>true, 'message'=>'get user post successfully', 'status'=>200]);
+        }catch (\Exception $e){
+            return response()->json(['data' => $e->getMessage(),'success'=>false,'message'=>$e->getMessage(),'status'=>404]);
+        }
     }
 }
---
+++ app/Http/Resources/UserPostsResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\UserPostsResource;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Models\Post;
+
+class UserPostsResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'posts_numbers' => Post::where('auth_id', $this->id)->count(),
+            'posts_content'=> Post::where('auth_id', $this->id)->get(),
+        ];
+    }
+}
