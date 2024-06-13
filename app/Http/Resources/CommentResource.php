6628a36c17207d4d3d2acd3d7e1d08c3979866c1
KHLORP.VEAK <KHLORP.VEAK@PNC.LAN>
2024-06-13 11:07:22 +0700

    [update comment G-14]

--- app/Http/Resources/CommentResource.php
+++ app/Http/Resources/CommentResource.php
@@ -21,11 +21,13 @@ class CommentResource extends JsonResource
             'id' => $this->id,
             'body' => $this->comment,
             'post_id' => $this->post->title,
+            'post_id' => $this->post->images,
             'user_name' => $this->user->name,
             'user_email' => $this->user->email,
             'user_image' => $this->user->image,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
+            'total_comment' => Comment::where('post_id', $this->post_id)->count(),
         ];
     }
 }
