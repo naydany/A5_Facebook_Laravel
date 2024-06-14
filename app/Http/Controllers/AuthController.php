8dc6c97138c6fe00c633767a1a1458a5be66b0b6
naydany <dany.nay@student.passerellesnumeriques.org>
2024-06-14 20:55:18 +0700

    reset_pwd LP-17

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,9 +16,6 @@ use App\Http\Resources\UserPostsResource\UserPostsResource;
 
 class AuthController extends Controller
 {
-
-    
-    
     public function register(Request $request){
         $request->validate([
             'name' => 'required',
@@ -128,8 +125,9 @@ class AuthController extends Controller
                 'message'=>'Old password does not matched',
             ],400);
         }
-        
     }
+
+    // ------ get user post
     public function user_posts()
     {
         $users = User::all();
