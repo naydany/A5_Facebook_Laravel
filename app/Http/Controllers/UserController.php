4fb416fd16b69f756cdc5ec536dbaf0d3eda353f
KHLORP.VEAK <KHLORP.VEAK@PNC.LAN>
2024-06-15 10:49:30 +0700

    merge form create_profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,39 +9,6 @@ use App\Http\Resources\UserResource;
 use Exception;
 use App\Http\Requests\UserRequest;
 
-/**
- * @OA\Post(
- *     path="/api/user",
- *     summary="User data",
- *     description="",
- *     tags={"User"},
- *     @OA\Parameter(
- *         name="name",
- *         in="query",
- *         description="Provide your name",
- *         required=true,
- *         @OA\Schema(
- *             type="string"
- *         )
- *     ),
- *     @OA\Response(
- *         response=200,
- *         description="OK",
- *         @OA\JsonContent(
- *             type="object",
- *             @OA\Property(
- *                 property="name",
- *                 type="string"
- *             ),
- *             @OA\Property(
- *                 property="message",
- *                 type="string"
- *             )
- *         )
- *     )
- * )
- */
-
 class UserController extends Controller
 {
     public function me(Request $request): JsonResponse
