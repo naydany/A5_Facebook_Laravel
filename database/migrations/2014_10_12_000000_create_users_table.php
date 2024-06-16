a5ae5c91035cbf9c3b0421c3ca80141220a23f70
naydany <dany.nay@student.passerellesnumeriques.org>
2024-06-16 13:12:58 +0700

    contioue_upadate_changepwd

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->integer('code')->nullable()->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->integer('code')->nullable();
             $table->string('profile_image')->nullable();;
             $table->string('password');
             $table->rememberToken();
--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,16 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\ForgetPassword;
+use App\Http\Controllers\ChangePassword\ForgetPassword;
+use App\Http\Controllers\ChangePassword\resetPasswordController;
+use App\Http\Controllers\ChangePassword\verifyCodeController as ChangePasswordVerifyCodeController;
 use App\Http\Controllers\LikesController;
 use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\CommentController;
-use App\Http\Controllers\TestController;
+
+
 
 // use App\Http\Controllers\LikeCommentController;
 
@@ -23,49 +25,48 @@ use App\Http\Controllers\TestController;
 |
 */
 
-Route::post('/test',[TestController::class,'test']);
 
 Route::prefix('auth')->group(function(){
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
     Route::get('/me', [AuthController::class, 'index'])->middleware('auth:sanctum');
     Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-    Route::post('/change_password',[AuthController::class,'change_password'])->middleware('auth:sanctum');
 
-    Route::post('/reset_password',[ForgetPassword::class,'sendWelcomeEmail']);
-    Route::post('/verify-code',[AuthController::class,'verify-code'])->middleware('auth:sanctum');
-    Route::post('/change_password',[AuthController::class,'change_password'])->middleware('auth:sanctum');
+    Route::post('/forgot_password',[ForgetPassword::class,'sendWelcomeEmail']);
+    Route::post('/verify-code',[ChangePasswordVerifyCodeController::class,'verifyCode'])->middleware('auth:sanctum');
+    Route::post('/change_password',[resetPasswordController::class,'changePassword'])->middleware('auth:sanctum');
 
     Route::get('/user/post', [AuthController::class, 'user_posts']);
     Route::get('/user/post/{id}', [AuthController::class, 'show_user_posts']);
 
 });
 
-Route::post('/add-like',[LikesController::class,'addLike'])->middleware('auth:sanctum');
-
 // group profile
-Route::group([
-   'middleware' => ['auth:sanctum']
-]
-, function () {
-    Route::get('/profile/detail', [ProfileController::class, 'profile'])->name('profile.view');
-    Route::post('/profile/update', [ProfileController::class, 'updateProfile'])->name('profile.update');
+Route::prefix('profile')->group(function(){
+    Route::group([
+       'middleware' => ['auth:sanctum']
+    ]
+    , function () {
+        Route::get('/detail', [ProfileController::class, 'profile'])->name('profile.view');
+        Route::post('/update', [ProfileController::class, 'updateProfile'])->name('profile.update');
+    });
 });
 
 //Comment to posts
-Route::prefix('auth')->group(function(){
-    Route::get('/comment/list', [CommentController::class, 'index']);
-    Route::post('/comment/create', [CommentController::class, 'store']);
-    Route::put('/comment/update/{id}', [CommentController::class, 'updateComment']);
-    Route::delete('/comment/delete/{id}', [CommentController::class, 'deleteComment']);
+Route::prefix('comment')->group(function(){
+    Route::get('/list', [CommentController::class, 'index']);
+    Route::post('/create', [CommentController::class, 'store']);
+    Route::put('/update/{id}', [CommentController::class, 'updateComment']);
+    Route::delete('/delete/{id}', [CommentController::class, 'deleteComment']);
 });
 
 // create posts
-Route::prefix('auth')->group(function(){
+Route::prefix('post')->group(function(){
     Route::get('/post', [PostController::class,'index']);
-    Route::get('/post/show/{id}', [PostController::class, 'show']);
-    Route::post('/post/create', [PostController::class, 'store']);
-    Route::put('/post/edit/{id}', [PostController::class, 'update']);
-    Route::delete('/post/delete/{id}', [PostController::class, 'destroy']);
-    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    Route::get('/show/{id}', [PostController::class, 'show']);
+    Route::post('/create', [PostController::class, 'store']);
+    Route::put('/edit/{id}', [PostController::class, 'update']);
+    Route::delete('/delete/{id}', [PostController::class, 'destroy']);
+
+    Route::post('/add-like',[LikesController::class,'addLike'])->middleware('auth:sanctum');
 });
